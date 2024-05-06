10260c3b0dd6c9fc64d50c23eded89232db1edf6
Finalspace <finalspace@gmail.com>
2024-05-06 11:46:21 +0200

    Fixed issues was not bindable

--- OnigiriCore/Models/Issues.cs
+++ OnigiriCore/Models/Issues.cs
@@ -1,32 +1,31 @@
 ﻿using DevExpress.Mvvm;
 using Finalspace.Onigiri.Enums;
-using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace Finalspace.Onigiri.Models
 {
     public class Issues : BindableBase
     {
-        public ObservableCollection<Issue> Items
-        {
-            get => GetValue<ObservableCollection<Issue>>();
-            set => SetValue(value);
-        }
+        public IReadOnlyCollection<Issue> Items => _items;
+        private ImmutableList<Issue> _items = ImmutableList<Issue>.Empty;
+
         public int Count => Items.Count;
 
         public Issues()
         {
-            Items = new ObservableCollection<Issue>();
-            Items.CollectionChanged += (s, e) => RaisePropertyChanged(() => Items);
         }
 
         public void Add(IssueKind kind, string message, string path, object value = null)
         {
-            Items.Add(new Issue(kind, message, path, value));
+            _items = _items.Add(new Issue(kind, message, path, value));
+            RaisePropertyChanged(nameof(Items));
         }
 
         public void Clear()
         {
-            Items.Clear();
+            _items = ImmutableList<Issue>.Empty;
+            RaisePropertyChanged(nameof(Items));
         }
     }
 }
