9507265e11cfcd8e751abca8f2710afe55f2dee1
Finalspace <finalspace@gmail.com>
2026-04-05 13:41:48 +0200

    Migrate OnigiriTests

--- OnigiriTests/AnimeSerializationTests.cs
+++ OnigiriTests/AnimeSerializationTests.cs
@@ -8,148 +8,145 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace OnigiriTests
+namespace OnigiriTests;
+
+[TestClass]
+public class AnimeSerializationTests
 {
-    [TestClass]
-    public class AnimeSerializationTests
+    [TestMethod]
+    public void SerializeTest()
     {
-        [TestMethod]
-        public void SerializeTest()
+        Anime source = new Anime()
         {
-            Anime source = new Anime()
-            {
-                Aid = 2337,
-                Description = "My description",
-                EpCount = 1,
-                StartDate = new DateTime(2002, 05, 07, 11, 33, 21),
-                EndDate = new DateTime(2003, 09, 05, 13, 05, 44),
-                Type = "OVA",
-            };
+            Aid = 2337,
+            Description = "My description",
+            EpCount = 1,
+            StartDate = new DateTime(2002, 05, 07, 11, 33, 21),
+            EndDate = new DateTime(2003, 09, 05, 13, 05, 44),
+            Type = "OVA",
+        };
 
-            source.Categories.Add(new Category()
-            {
-                Id = 1,
-                Description = "Extreme violence",
-                Name = "Violence",
-                ParentId = 0,
-                Weight = 10,
-            });
-            source.Categories.Add(new Category()
-            {
-                Id = 2,
-                Description = "Comedy",
-                Name = "Comedy",
-                ParentId = 0,
-                Weight = 15,
-            });
-            source.Categories.Add(new Category()
-            {
-                Id = 2,
-                Description = "Bad people",
-                Name = "Bad",
-                ParentId = 0,
-                Weight = 40,
-            });
+        source.Categories.Add(new Category()
+        {
+            Id = 1,
+            Description = "Extreme violence",
+            Name = "Violence",
+            ParentId = 0,
+            Weight = 10,
+        });
+        source.Categories.Add(new Category()
+        {
+            Id = 2,
+            Description = "Comedy",
+            Name = "Comedy",
+            ParentId = 0,
+            Weight = 15,
+        });
+        source.Categories.Add(new Category()
+        {
+            Id = 2,
+            Description = "Bad people",
+            Name = "Bad",
+            ParentId = 0,
+            Weight = 40,
+        });
 
-            source.Ratings.Add(new Rating()
-            {
-                Count = 1024,
-                Name = RatingTypes.Pernament,
-                Value = 7.32,
-            });
+        source.Ratings.Add(new Rating()
+        {
+            Count = 1024,
+            Name = RatingTypes.Pernament,
+            Value = 7.32,
+        });
 
-            source.Episodes.Add(new Episode()
-            {
-                Id = 1,
-                AirDate = DateTime.Now,
-                Length = 333,
-                Num = "First",
-                NumType = 0,
-                Rating = new Rating() { Count = 233, Name = RatingTypes.Pernament, Value = 4.3 },
-                Titles = new ObservableCollection<Title>(),
-            });
+        source.Episodes.Add(new Episode()
+        {
+            Id = 1,
+            AirDate = DateTime.Now,
+            Length = 333,
+            Num = "First",
+            NumType = 0,
+            Rating = new Rating() { Count = 233, Name = RatingTypes.Pernament, Value = 4.3 },
+            Titles = new ObservableCollection<Title>(),
+        });
 
-            source.Titles.Add(new Title()
-            {
-                Aid = source.Aid,
-                Lang = LanguageNames.EnglishShort,
-                Type = TitleTypes.Main,
-                Name = "The anime name",
-            });
+        source.Titles.Add(new Title()
+        {
+            Aid = source.Aid,
+            Lang = LanguageNames.EnglishShort,
+            Type = TitleTypes.Main,
+            Name = "The anime name",
+        });
 
-            IEnumerable<Category> topCats = source.Categories.OrderByDescending(d => d.Weight).Take(Math.Min(10, source.Categories.Count));
-            foreach (Category item in topCats)
-                source.TopCategories.Add(item);
+        IEnumerable<Category> topCats = source.Categories.OrderByDescending(d => d.Weight).Take(Math.Min(10, source.Categories.Count));
+        foreach (Category item in topCats)
+            source.TopCategories.Add(item);
 
-            var mediaFile = new AnimeMediaFile()
+        var mediaFile = new AnimeMediaFile()
+        {
+            FileName = "the_anime.avi",
+            FileSize = 1337,
+            Info = new MediaInfo()
             {
-                FileName = "the_anime.avi",
-                FileSize = 1337,
-                Info = new MediaInfo()
+                Audio = new List<AudioInfo>(new[]
                 {
-                    Audio = new List<AudioInfo>(new[]
+                    new AudioInfo()
                     {
-                        new AudioInfo()
-                        {
-                            Channels = 2,
-                            SampleRate = 44100,
-                            FrameCount = 2337,
-                            BitRate = 128,
-                            BitsPerSample = 16,
-                            Codec = new CodecDescription(FourCC.FromString("mp3"), "Mpeg layer-3"),
-                            Lang = LanguageNames.JapaneseShort,
-                            Name = "AudioTrack",
-                        }
-                    }),
-                    Video = new List<VideoInfo>(new[]
+                        Channels = 2,
+                        SampleRate = 44100,
+                        FrameCount = 2337,
+                        BitRate = 128,
+                        BitsPerSample = 16,
+                        Codec = new CodecDescription(FourCC.FromString("mp3"), "Mpeg layer-3"),
+                        Lang = LanguageNames.JapaneseShort,
+                        Name = "AudioTrack",
+                    }
+                }),
+                Video = new List<VideoInfo>(new[]
+                {
+                    new VideoInfo()
                     {
-                        new VideoInfo()
-                        {
-                            FrameCount = 289,
-                            Codec = new CodecDescription(FourCC.FromString("xvid"), "XViD"),
-                            Name = "VideoTrack",
-                            FrameRate = 23.335,
-                            Height = 720,
-                            Width = 1280,
-                        }
-                    }),
-                    Subtitles = new List<SubtitleInfo>(new[]
+                        FrameCount = 289,
+                        Codec = new CodecDescription(FourCC.FromString("xvid"), "XViD"),
+                        Name = "VideoTrack",
+                        FrameRate = 23.335,
+                        Height = 720,
+                        Width = 1280,
+                    }
+                }),
+                Subtitles = new List<SubtitleInfo>(new[]
+                {
+                    new SubtitleInfo()
                     {
-                        new SubtitleInfo()
-                        {
-                            Name = "Subtitle",
-                            Lang = LanguageNames.EnglishShort,
-                        }
-                    })
-                },
-            };
+                        Name = "Subtitle",
+                        Lang = LanguageNames.EnglishShort,
+                    }
+                })
+            },
+        };
 
-            source.ExtendedMediaFiles.Add(mediaFile);
+        source.ExtendedMediaFiles.Add(mediaFile);
 
-            source.MediaFiles = new ObservableCollection<string>(source.ExtendedMediaFiles.Select(s => s.FileName));
+        source.MediaFiles = new ObservableCollection<string>(source.ExtendedMediaFiles.Select(s => s.FileName));
 
-            Anime target;
-            using (MemoryStream stream = AnimeSerialization.SerializeAnime(source))
-                target = AnimeSerialization.DeserializeAnime(stream.GetBuffer(), source.Aid);
+        Anime target;
+        using (MemoryStream stream = AnimeSerialization.SerializeAnime(source))
+            target = AnimeSerialization.DeserializeAnime(stream.GetBuffer(), source.Aid);
 
-            Assert.IsNotNull(target);
-            Assert.AreEqual(source.Aid, target.Aid);
-            Assert.AreEqual(source.MainTitle, target.MainTitle);
+        Assert.IsNotNull(target);
+        Assert.AreEqual(source.Aid, target.Aid);
+        Assert.AreEqual(source.MainTitle, target.MainTitle);
 
-            Assert.AreEqual(source.MediaFiles.Count, target.MediaFiles.Count);
-            for (int i = 0; i < source.MediaFiles.Count; i++)
-                Assert.AreEqual(source.MediaFiles[i], target.MediaFiles[i]);
+        Assert.AreEqual(source.MediaFiles.Count, target.MediaFiles.Count);
+        for (int i = 0; i < source.MediaFiles.Count; i++)
+            Assert.AreEqual(source.MediaFiles[i], target.MediaFiles[i]);
 
-            Assert.AreEqual(source.ExtendedMediaFiles.Count, target.ExtendedMediaFiles.Count);
-            for (int i = 0; i < source.ExtendedMediaFiles.Count; i++)
-            {
-                AnimeMediaFile sourceMediaFile = source.ExtendedMediaFiles[i];
-                AnimeMediaFile targetExtendedMediaFile = target.ExtendedMediaFiles[i];
-                Assert.AreEqual(sourceMediaFile, targetExtendedMediaFile);
-            }
+        Assert.AreEqual(source.ExtendedMediaFiles.Count, target.ExtendedMediaFiles.Count);
+        for (int i = 0; i < source.ExtendedMediaFiles.Count; i++)
+        {
+            AnimeMediaFile sourceMediaFile = source.ExtendedMediaFiles[i];
+            AnimeMediaFile targetExtendedMediaFile = target.ExtendedMediaFiles[i];
+            Assert.AreEqual(sourceMediaFile, targetExtendedMediaFile);
         }
     }
 }
--- OnigiriTests/MediaParserTests.cs
+++ OnigiriTests/MediaParserTests.cs
@@ -7,70 +7,69 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace OnigiriTests
-{
-    [TestClass]
-    public class MediaParserTests
-    {
-        static IEnumerable<FileInfo> AddFilesFromFolder(DirectoryInfo rootDir)
-        {
-            if (!rootDir.Attributes.HasFlag(FileAttributes.System))
-            {
-                FileInfo[] subFiles = rootDir.GetFiles();
-                foreach (FileInfo subFile in subFiles)
-                {
-                    if (subFile.Attributes.HasFlag(FileAttributes.System))
-                        continue;
-                    string ext = subFile.Extension.ToLower();
-                    if (OnigiriService.MediaFileExtensions.Contains(ext))
-                        yield return subFile;
-                }
+namespace OnigiriTests;
 
-                DirectoryInfo[] subdirs = rootDir.GetDirectories();
-                foreach (DirectoryInfo subdir in subdirs)
-                {
-                    IEnumerable<FileInfo> subfiles = AddFilesFromFolder(subdir);
-                    foreach (FileInfo subfile in subfiles)
-                        yield return subfile;
-                }
+[TestClass]
+public class MediaParserTests
+{
+    static IEnumerable<FileInfo> AddFilesFromFolder(DirectoryInfo rootDir)
+    {
+        if (!rootDir.Attributes.HasFlag(FileAttributes.System))
+        {
+            FileInfo[] subFiles = rootDir.GetFiles();
+            foreach (FileInfo subFile in subFiles)
+            {
+                if (subFile.Attributes.HasFlag(FileAttributes.System))
+                    continue;
+                string ext = subFile.Extension.ToLower();
+                if (OnigiriService.MediaFileExtensions.Contains(ext))
+                    yield return subFile;
+            }
+
+            DirectoryInfo[] subdirs = rootDir.GetDirectories();
+            foreach (DirectoryInfo subdir in subdirs)
+            {
+                IEnumerable<FileInfo> subfiles = AddFilesFromFolder(subdir);
+                foreach (FileInfo subfile in subfiles)
+                    yield return subfile;
             }
         }
+    }
 
-        [TestMethod]
-        public void TestAvi()
+    [TestMethod]
+    public void TestAvi()
+    {
+        string usersPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+        string folderPath = Path.Combine(usersPath, "OneDrive", "Q3");
+
+        DirectoryInfo folder = new DirectoryInfo(folderPath);
+        if (!folder.Exists)
+            Assert.Fail($"Folder path '{folderPath}' does not exists");
+
+        List<FileInfo> files = AddFilesFromFolder(folder).ToList();
+
+        foreach (FileInfo file in files)
         {
-            string usersPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            Task<MediaInfo> task = MediaInfoParser.Parse(file);
+            task.Wait();
+            MediaInfo info = task.Result;
+            Assert.IsNotNull(info);
 
-            string folderPath = Path.Combine(usersPath, "OneDrive", "Q3");
+            Console.WriteLine(FormattableString.Invariant($"\tContainer: '{info.Format}', Duration: {info.Duration.TotalSeconds} secs"));
 
-            DirectoryInfo folder = new DirectoryInfo(folderPath);
-            if (!folder.Exists)
-                Assert.Fail($"Folder path '{folderPath}' does not exists");
-
-            List<FileInfo> files = AddFilesFromFolder(folder).ToList();
-
-            foreach (FileInfo file in files)
+            foreach (VideoInfo video in info.Video)
             {
-                Task<MediaInfo> task = MediaInfoParser.Parse(file);
-                task.Wait();
-                MediaInfo info = task.Result;
-                Assert.IsNotNull(info);
+                Assert.IsNotNull(video);
+                Console.WriteLine(FormattableString.Invariant($"\tVideo: {video.Width}x{video.Height}, {video.FrameCount} frames, {video.FrameRate} fps [Codec:'{video.Codec}', Name: '{video.Name}']"));
+                Assert.IsTrue(video.Width > 0 && video.Height > 0 && !video.Codec.Id.IsEmpty);
+            }
 
-                Console.WriteLine(FormattableString.Invariant($"\tContainer: '{info.Format}', Duration: {info.Duration.TotalSeconds} secs"));
-
-                foreach (VideoInfo video in info.Video)
-                {
-                    Assert.IsNotNull(video);
-                    Console.WriteLine(FormattableString.Invariant($"\tVideo: {video.Width}x{video.Height}, {video.FrameCount} frames, {video.FrameRate} fps [Codec:'{video.Codec}', Name: '{video.Name}']"));
-                    Assert.IsTrue(video.Width > 0 && video.Height > 0 && !video.Codec.Id.IsEmpty);
-                }
-
-                foreach (AudioInfo audio in info.Audio)
-                {
-                    Assert.IsNotNull(audio);
-                    Console.WriteLine(FormattableString.Invariant($"\tAudio: {audio.Channels} channels, {audio.SampleRate} Hz, {audio.BitsPerSample} bits/sample, {audio.BitRate / 1000} kHz [Codec: '{audio.Codec}', Name: '{audio.Name}']"));
-                    Assert.IsTrue(audio.Channels > 0 && audio.SampleRate > 0 && !audio.Codec.Id.IsEmpty);
-                }
+            foreach (AudioInfo audio in info.Audio)
+            {
+                Assert.IsNotNull(audio);
+                Console.WriteLine(FormattableString.Invariant($"\tAudio: {audio.Channels} channels, {audio.SampleRate} Hz, {audio.BitsPerSample} bits/sample, {audio.BitRate / 1000} kHz [Codec: '{audio.Codec}', Name: '{audio.Name}']"));
+                Assert.IsTrue(audio.Channels > 0 && audio.SampleRate > 0 && !audio.Codec.Id.IsEmpty);
             }
         }
     }
